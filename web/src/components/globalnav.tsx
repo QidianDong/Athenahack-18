4ae955acc1593e0cfdb39a49d5e855aa611b4971
No767 <73260931+No767@users.noreply.github.com>
2025-03-02 12:13:17 -0800

    implement web ui for demo purposes

---
+++ web/src/components/globalnav.tsx
@@ -0,0 +1,43 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+
+import { Link } from "react-router";
+
+import { cn } from "@/lib/utils"
+import { Icon } from "@iconify/react";
+
+
+function GlobalNavbar() {
+    return (
+        <>
+      <NavigationMenu className="mx-auto flex flex-row justify-end max-w-5xl pt-4 px-4">
+
+        <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+              <NavigationMenuLink>
+                Documentation 2
+              </NavigationMenuLink></Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+          <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+              <NavigationMenuLink>
+                Documentation
+              </NavigationMenuLink></Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+        
+        </>
+    )
+}
+
+export {GlobalNavbar} ;
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,8 +1,29 @@
+import { Textarea } from "@/components/ui/textarea"
+import { GlobalNavbar } from "../components/globalnav";
+import { Button } from "@/components/ui/button";
+
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <>
+  
+    <div className="bg-[url(assets/mesh-gradient.png)] bg-center bg-auto h-screen backdrop-opacity-90 backdrop-blur-md">
+    <GlobalNavbar/>
+
+
+    <h1 className="mt-16 scroll-m-20 text-4xl font-extrabold tracking-normal lg:text-5xl text-center">
+        Insert Gene
+      </h1> 
+    <h3 className="mt-4 scroll-m-2 text-1xl font-extrabold tracking-normal lg:text-3xl text-center">
+      yooo
+    </h3>
+    <div className="py-8 grid  h-3xl w-2xl gap-2  justify-self-center">
+      <Textarea placeholder="Type or copy and paste your content here" />
+      <Button>Analyze!</Button>
     </div>
+    </div>
+
+
+    </>
   );
 }
 
